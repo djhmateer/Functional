84b832a98871b456e456d5cd149237ced7428f16
Dave Mateer <davemateer@gmail.com>
2014-10-21 06:59:48 +0100

    Got Higher order function - Find working.

--- csharp-func/Class1.cs
+++ csharp-func/Class1.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace csharp_func {
     internal static class Program2 {
         private static void Main(string[] args) {
             var numbers = new[] { 3, 5, 7, 9, 11, 13, 15 };
-            foreach (var prime in numbers.FindPrimes()) {
+            // Passing in the IsPrime function into Find
+            foreach (var prime in numbers.Find(IsPrime)) {
                 Console.WriteLine(prime);
             }
         }
 
-        private static IEnumerable<int> FindPrimes(this IEnumerable<int> values) {
+        // Find is a higher order function, as it takes another function as a parameter
+        // composable with other functions
+        private static IEnumerable<int> Find(this IEnumerable<int> values, Func<int, bool> test) {
             //iterate
             //test if prime
             //return a difference sequence of ints
             var result = new List<int>();
             foreach (var number in values) {
-                if (IsPrime(number)) {
+                //if (IsPrime(number)) {
+                if (test(number)) {
                     result.Add(number);
                 }
             }
